fe1ac7e6ea7455c65de59e7b4bef22d8adcb7e9f
Satyam Adhikary <satyamadhikary226@gmail.com>
2024-04-10 21:34:19 +0530

    favicon

--- script.js
+++ script.js
@@ -152,18 +152,11 @@ function videoScroll() {
         videoEl = document.querySelectorAll('video[autoplay]');
 
     for (var i = 0; i < videoEl.length; i++) {
+        var thisVideoEl = videoEl[i],
+            videoHeight = thisVideoEl.clientHeight,
+            videoClientRect = thisVideoEl.getBoundingClientRect().top;
 
-      var thisVideoEl = videoEl[i],
-          videoHeight = thisVideoEl.clientHeight,
-          videoClientRect = thisVideoEl.getBoundingClientRect().top;
-
-      if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
-        thisVideoEl.play();
-      } else {
-        thisVideoEl.pause();
-      }
-
+        videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ? thisVideoEl.play() : thisVideoEl.pause();
     }
   }
-
 }
